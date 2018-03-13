0a62a801607e184ebf25b1be79f1da90c6dedba1
Alfredo Reyes <benitoalfredoreyes@gmail.com>
2018-03-12 23:32:51 -0600

    Show image in main form

--- uimagen.pas
+++ uimagen.pas
@@ -7,7 +7,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
-  ExtCtrls,uVarios,uHistograma;
+  ExtCtrls,uVarios,uHistograma,Math;
 
 type
 
@@ -15,6 +15,7 @@ type
 
   TfrmImagen = class(TForm)
     Image1: TImage;
+    imgHisto: TImage;
     MainMenu1: TMainMenu;
     mnuHerrHistograma: TMenuItem;
     mnuHerramientas: TMenuItem;
@@ -29,6 +30,7 @@ type
     procedure mnuArchivoSalirClick(Sender: TObject);
     procedure MImagen (B : TBitMap);
     procedure mnuHerrHistogramaClick(Sender: TObject);
+    procedure PintaHisto();
   private
     { private declarations }
   public
@@ -36,6 +38,10 @@ type
     BM : TBitMap;
     Iancho, Ialto : integer;
     MTR, MRES : Mat3D;
+    Han, Hal, nc, nr : integer;
+    BH : TBitMap;
+    MH : Mat3D;
+    HH : ArrInt;
   end;
 
 var
@@ -63,6 +69,14 @@ begin
     nom := OpenDialog1.FileName;
     BM.LoadFromFile(nom);  //Carga la imagen en el BitMap
     MImagen(BM);           //Muestra la imagen
+
+    BA.Assign(frmImagen.Image1.Picture.Bitmap);
+
+    BH.Assign(BA);  //El contenido de BA se asigna a BH
+    nc := BH.Width; //Número de columnas
+    nr := BH.Height;//Número de renglones
+    BM_MAT(BH,MH);  //Pasa la imagen a un matriz
+    PintaHisto();   //Pinta el histograma
   end;
 
 end;
@@ -71,6 +85,11 @@ procedure TfrmImagen.FormCreate(Sender: TObject);
 begin
   BM := TBitmap.Create;
   BA := TBitmap.Create;
+    Han := imgHisto.Width;  //Ancho y alto de la ventana donde se
+  Hal := imgHisto.Height; //graficara el histograma
+  imgHisto.Canvas.Brush.Color := clWhite;
+  imgHisto.Canvas.Rectangle(0,0,Han,Hal);  //Dibuja un rectángulo negro
+  BH := TBitMap.Create; //Se crea el área de trabajo
 end;
 
 //Muestra el contenido del BitMap en el Image
@@ -86,5 +105,82 @@ begin
   frmHistograma.Show;
 end;
 
+//Pinta el histograma
+procedure TfrmImagen.PintaHisto();
+var
+  i,j,ind : integer;
+  maxi     : array [0..3] of integer;
+  fac      : real;
+begin
+  for i := 0 to 255 do  //limpia la matriz
+  begin
+     HH[0,i] := 0;
+     HH[1,i] := 0;
+     HH[2,i] := 0;
+     HH[3,i] := 0;
+  end;
+  //obtiene la cantidad de cada color 0..255
+  for i:= 0 to nc-1 do
+    for j := 0 to nr-1 do
+    begin
+      ind := MH[i][j][0]; //Rojo
+      inc(HH[0,ind]);
+      ind := MH[i][j][1]; //Verde
+      inc(HH[1,ind]);
+      ind := MH[i][j][2]; //Azul
+      inc(HH[2,ind]);
+      ind :=  (MH[i][j][0] + MH[i][j][1] + MH[i][j][2]) div 3;
+      inc(HH[3,ind]);     //Gris
+    end;
+  // pinta
+  maxi[0] := 0;
+  maxi[1] := 0;
+  maxi[2] := 0;
+//  maxi[3] := 0;
+  {*if chkRojo.Checked then
+  begin*}
+    imgHisto.Canvas.Pen.Color := clRed;      //Rojo
+    for i := 0 to 255 do
+      maxi[0] := Max(HH[0,i], maxi[0]);
+    fac := Hal / (maxi[0]+1);
+    imgHisto.Canvas.MoveTo(0,Hal-round(fac*HH[0,0]));
+    for i := 1 to 255 do
+
+      imgHisto.Canvas.LineTo(round(i * Han/255), Hal-round(fac*HH[0,i]));
+  //end;
+  {*if chkVerde.Checked then
+  begin*}
+    imgHisto.Canvas.Pen.Color := clGreen;   //Verde
+    for i := 0 to 255 do
+      maxi[1] := Max(HH[1,i], maxi[1]);
+    fac := Hal / (maxi[1]+1);
+    imgHisto.Canvas.MoveTo(0,Hal-round(fac*HH[1,0]));
+    for i := 1 to 255 do
+
+      imgHisto.Canvas.LineTo(round(i * Han/255), Hal-round(fac*HH[1,i]));
+  {*end;
+  if chkAzul.Checked then
+  begin*}
+    imgHisto.Canvas.Pen.Color := clBlue;    //Azul
+    for i := 0 to 255 do
+      maxi[2] := Max(HH[2,i], maxi[2]);
+    fac := Hal / (maxi[2]+1);
+    imgHisto.Canvas.MoveTo(0,Hal-round(fac*HH[2,0]));
+    for i := 1 to 255 do
+
+      imgHisto.Canvas.LineTo(round(i * Han/255), Hal-round(fac*HH[2,i]));
+{*  end;
+  if chkGris.Checked then
+  begin*}
+    imgHisto.Canvas.Pen.Color := clWhite;    //Gris
+    for i := 0 to 255 do
+      maxi[3] := Max(HH[3,i], maxi[3]);
+    fac := Hal / (maxi[3]+1);
+    imgHisto.Canvas.MoveTo(0,Hal-round(fac*HH[3,0]));
+    for i := 1 to 255 do
+
+      imgHisto.Canvas.LineTo(round(i * Han/255), Hal-round(fac*HH[3,i]));
+  {*end;*}
+end;
 end.
 
